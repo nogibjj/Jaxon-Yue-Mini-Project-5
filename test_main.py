01a021ca742bcf10d23c6be03e0bf0a069f1edcc
jaxonyue <70416390+jaxonyue@users.noreply.github.com>
2023-10-02 18:01:45 +0000

    commit

--- test_main.py
+++ test_main.py
@@ -1,5 +1,7 @@
-from main import main
+import sys # noqa: E402
+sys.path.append('➜ /workspaces/Jaxon-Yue-Mini-Project-5/lib') # noqa: E402
 
+from main import main # noqa: E402
 
 def test_main():
     assert main() == 1
