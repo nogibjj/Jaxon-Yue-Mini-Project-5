3be1ff87f1c0d42c7457905916b989029e87396a
jaxonyue <70416390+jaxonyue@users.noreply.github.com>
2023-10-02 18:42:25 +0000

    test

--- test_main.py
+++ test_main.py
@@ -1,7 +1,7 @@
-import sys # noqa: E402
-sys.path.append('➜ /workspaces/Jaxon-Yue-Mini-Project-5/lib') # noqa: E402
-
-from main import main # noqa: E402
+from main import main
 
 def test_main():
     assert main() == 1
+
+if __name__ == '__main__':
+    test_main()
